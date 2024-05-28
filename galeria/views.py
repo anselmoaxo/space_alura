2a41667dc997784e9fa67537d45ac96d6ec87d08
Anselmo Xavier <anselmo.cstecnologia@gmail.com>
2024-05-28 11:52:08 -0300

    adicionando imagens via urls

--- galeria/views.py
+++ galeria/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia
 
 
@@ -8,6 +8,8 @@ def index(request):
     return render(request,  'galeria/index.html', contexto)
 
 
-def imagem(request):
-    
-    return render(request,  'galeria/imagem.html')
+def imagem(request, foto_id):
+    # get object or 404 busca o id caso não encontre retorno o erro 404
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    contexto = {'fotografia': fotografia}
+    return render(request,  'galeria/imagem.html', contexto)
