6edd1d55e2ebac3c4841ea8dc421f1043a8a82a8
Anselmo <anselmo.cstecnologia@gmail.com>
2024-05-28 22:39:02 -0300

    adicionando busca no formulario

--- galeria/views.py
+++ galeria/views.py
@@ -13,3 +13,18 @@ def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     contexto = {'fotografia': fotografia}
     return render(request,  'galeria/imagem.html', contexto)
+
+def buscar(request):
+    fotografias = Fotografia.objects.order_by("-data_foto").filter(publicada=True)
+    if 'buscar' in request.GET:
+        pesquisa = request.GET['buscar']
+        if pesquisa:
+            fotografias = fotografias.filter(nome__icontains=pesquisa)
+            
+        else:
+            fotografias = Fotografia.objects.order_by("-data_foto").filter(publicada=True)
+        
+        contexto = {'cards': fotografias}
+
+
+    return render(request, 'galeria/buscar.html', contexto)
