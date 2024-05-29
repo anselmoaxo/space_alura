3d65fc8718d7e6fef1d8b70a2f8c73975e9c12a3
Anselmo <anselmo.cstecnologia@gmail.com>
2024-05-29 17:09:12 -0300

    configurando widget dos Forms

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,15 +1,22 @@
-from django.forms import forms
+from django import forms
 
 
 class LoginForms(forms.Form):
-    nome_login = forms.CharField(
-        label= 'Nome de Login',
-        max_length=50, 
-        required=True)
-    senha = forms.Charfiled(
-        label= 'Senha',
-        max_length=57, 
-        Widget= forms.PasswordInput()
-        required=True)
+    nome_login=forms.CharField(label= 'Nome de Login',
+                                 max_length=50, 
+                                 required=True,
+                                 widget=forms.TextInput(
+                                     attrs={
+                                    "class": "form-control",
+                                     "placeholder": "Digite seu Login"
+            }
+                                    ))
+    senha = forms.CharField(label= 'Senha',
+                             max_length=57, 
+                             required=True,
+                             widget= forms.PasswordInput(attrs={"class": "form-control",
+                                                                "placeholder": "Digite sua senha"}
+                                                         )
+                                                         )
 
     
